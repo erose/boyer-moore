344e2fee1012d4531deeface672303969e3b71e2
Eli Rose <really.eli@gmail.com>
2020-04-26 10:09:54 -0400

    Attempt to burn it all away; not working yet.

--- boyer-moore.py
+++ boyer-moore.py
@@ -2,147 +2,50 @@ from typing import *
 import enum
 import collections
 
-class Direction(enum.Enum):
-    forwards = 'forwards'
-    forwards_with_needle_index = 'forwards_with_needle_index'
-    backwards = 'backwards'
-
-def search(needle, haystack, direction):
-    if direction == Direction.backwards:
-        needle_backwards_index = build_backwards_index(needle)
-        haystack_position = len(needle) - 1
-    if direction == Direction.forwards:
-        # Subtle: this is a specialization of a forward index because the algo
-        # needs to jump to the last character in a run before jumping over the
-        # whole run.
-        #
-        # Note: This is *not* the same as a backwards index! For backwards
-        # indices, the 0 value is at the tail of the string. Here the 0 value is
-        # at the head of the string, but for in the case of runs, the index will
-        # skip us to the _last_ character in the run.
-        #
-        # search('aba', 'aaba'):
-        #
-        # index: {'a': 1, 'b': 2}
-        haystack_forwards_index = build_forwards_index(
-            haystack, run_strategy=RunStrategy.prefer_last)
-        haystack_position = 0
-    if direction == Direction.forwards_with_needle_index:
-        needle_forwards_index = build_forwards_index(needle)
-        haystack_position = 0
+def horspool_search(needle, haystack):
+    needle_index = build_index(needle)
+    print(needle_index)
+    p = len(needle) - 1
 
     while True:
         # If we've run off the end of the haystack, we know the string is not to be found.
-        if haystack_position >= len(haystack):
+        if p >= len(haystack):
             return False
 
-        if direction == Direction.backwards:
-            result, jump_ahead_by = compare_from_back(needle, haystack, haystack_position, needle_backwards_index)
-        if direction == Direction.forwards:
-            result, jump_ahead_by = compare_from_front(needle, haystack, haystack_position, haystack_forwards_index)
-        if direction == Direction.forwards_with_needle_index:
-            result, jump_ahead_by = compare_from_front(needle, haystack, haystack_position, needle_forwards_index)
-        if result is True:
+        print(needle, haystack[(p - len(needle) + 1):p+1])
+        if needle == haystack[(p - len(needle) + 1):p+1]:
             return True
         else:
-            haystack_position += jump_ahead_by
+            # Look at the last character of the slice of haystack we're focused on, find the index
+            # of its last ocurrence in the needle-with-last-character-chopped-off, and jump forward
+            # by enough to make sure these are aligned.
+            # 
+            # TODO: explain why
+            #
+            # abaaabbababcabdacbaabababc   -->   abaaabbababcabdacbaabababc
+            #            -                                     -
+            #     abdacabaabd              -->               abdacabaabd
+            #            -                                     -
+            last_haystack_char = haystack[p]
+            print("p", p)
+            print("last_haystack_char", last_haystack_char)
+            jump_ahead_by = needle_index.get(last_haystack_char, len(needle))
+            jump_ahead_by = max(jump_ahead_by, 1) # Always need to make some progress.
 
-def build_backwards_index(s) -> Dict[str, int]:
+            print("jump_ahead_by", jump_ahead_by)
+            p += jump_ahead_by
+
+def build_index(s) -> Dict[str, int]:
     """
-    Returns a dictionary which maps each character in s to its index, starting from the back of the
-    string. (e.g. the final character is at index 0).
+    Returns a dictionary which maps each character in s except the last one to the index of its
+    last ocurrence.
     """
     result = {}
-    for i, c in enumerate(reversed(s)):
-        if c in result:
-            continue
+
+    for i, c in enumerate(s[:-1]):
         result[c] = i
-
+    
     return result
-
-class RunStrategy(enum.Enum):
-    """How to handle runs of the same letter."""
-    # aaa
-    # ^
-    # use this one
-    prefer_first = 'prefer_first'
-
-    # aaa
-    #   ^
-    #   use this one
-    prefer_last = 'prefer_last'
-
-def build_forwards_index(
-    s, run_strategy=RunStrategy.prefer_first) -> Dict[str, int]:
-    """
-    Returns a dictionary which maps each character in s to its index, starting from the front of the
-    string. (e.g. the first character is at index 0).
-    """
-    result = {}
-
-    last_seen_letter = None
-    for i, c in enumerate(s):
-        if c in result:
-            if run_strategy == RunStrategy.prefer_first:
-                continue
-            elif run_strategy == RunStrategy.prefer_last:
-                if c == last_seen_letter:
-                    result[c] = i
-        else:
-          result[c] = i
-        last_seen_letter = c
-
-    return result
-
-def compare_from_back(needle, haystack, haystack_position, needle_backwards_index) -> Tuple[int, bool]:
-    for k in range(0, len(needle)):
-        needle_char = needle[-(k + 1)]
-        haystack_char = haystack[haystack_position - k]
-
-        if needle_char != haystack_char:
-            # Snap to the next ocurrence of this character.
-            # e.g.
-            #
-            # aba     -->   aba
-            # -             -
-            #
-            # cbacba  --> cbacba
-            # -             -
-            #
-            jump_ahead_by = needle_backwards_index.get(haystack_char, len(needle))
-
-            # Dan's claim: if we do more work on the index building, we can be smarter here.
-            jump_ahead_by = max(1, jump_ahead_by)
-            return False, jump_ahead_by
-
-    return True, None
-
-def compare_from_front(needle, haystack, haystack_position, haystack_forwards_index) -> Tuple[int, bool]:
-    # If we run out of haystack before we run out of needle, we can't match.
-    if len(needle) > len(haystack) - haystack_position:
-        return False, len(needle)
-
-    for k in range(0, len(needle)):
-        needle_char = needle[k]
-        haystack_char = haystack[haystack_position + k]
-
-        if needle_char != haystack_char:
-            # Snap to the next ocurrence of this character.
-            # e.g.
-            #
-            # aba     -->   aba
-            # -             -
-            #
-            # cbacba  --> cbacba
-            # -             -
-            #
-            jump_ahead_by = haystack_forwards_index.get(needle_char, len(haystack))
-
-            # Dan's claim: if we do more work on the index building, we can be smarter here.
-            jump_ahead_by = max(1, jump_ahead_by)
-            return False, jump_ahead_by
-
-    return True, None
 
 # Whole different search algorithms, for comparison.
 def naive_search(needle, haystack):
@@ -195,56 +98,28 @@ class BranchingSearchTestCase(unittest.TestCase):
         self.assertFalse(branching_search("abaaaaa", "baabac")) # Needle longer than haystack.
 
 class TestBuildIndexes(unittest.TestCase):
-    def test_build_backwards_index(self):
-        self.assertEqual(build_backwards_index("asab"), {'b': 0, 'a': 1, 's': 2})
+    def test_build_index(self):
+        self.assertEqual(build_index("aasab"), {'a': 3, 's': 2})
+        self.assertEqual(build_index("aaba"), {'a': 1, 'b': 2})
 
-    def test_build_forwards_index_prefer_first_in_run(self):
-        self.assertEqual(
-            build_forwards_index("aasab", run_strategy=RunStrategy.prefer_first),
-            {'a': 0, 's': 2, 'b': 4})
-
-        self.assertEqual(
-            build_forwards_index("aaba", run_strategy=RunStrategy.prefer_first),
-            {'a': 0, 'b': 2})
-
-    def test_build_forwards_index_prefer_last_in_run(self):
-        self.assertEqual(
-            build_forwards_index("aasab", run_strategy=RunStrategy.prefer_last),
-            {'a': 1, 's': 2, 'b': 4})
-
-        self.assertEqual(
-            build_forwards_index("aaba", run_strategy=RunStrategy.prefer_last),
-            {'a': 1, 'b': 2})
-
-class BoyerMooreTestCase(unittest.TestCase):
-    def do_test_for_direction(self, direction):
-        self.assertTrue(search("aba", "baabac", direction))
-        self.assertTrue(search("cab", "xyzcab", direction))
-        self.assertTrue(search("cab", ("xyz" * 100) + "cab", direction))
-        self.assertTrue(search("a", "baabac", direction))
-        self.assertTrue(search("apple", "xxxxaapple", direction))
-
-        self.assertFalse(search("abx", "baabac", direction))
-        self.assertFalse(search("abaaaaa", "baabac", direction)) # Needle longer than haystack.
-
-class TestForwardsBoyerMoore(BoyerMooreTestCase):
+class HorspoolTestCase(unittest.TestCase):
     def test_it_works(self):
-        self.do_test_for_direction(Direction.forwards)
+        # self.assertTrue(horspool_search("aba", "baabac"))
+        # self.assertTrue(horspool_search("cab", "xyzcab"))
+        # self.assertTrue(horspool_search("cab", ("xyz" * 100) + "cab"))
+        # self.assertTrue(horspool_search("a", "baabac"))
+        self.assertTrue(horspool_search("apple", "xxxxaapple"))
+        # self.assertTrue(horspool_search("cdc", "bcdcba"))
+        # self.assertTrue(horspool_search("ccc", "bcccba"))
 
-class TestForwardsWithNeedleIndexBoyerMoore(BoyerMooreTestCase):
-    def test_it_works(self):
-        self.do_test_for_direction(Direction.forwards_with_needle_index)
-
-class TestBackwardsBoyerMoore(BoyerMooreTestCase):
-    def test_it_works(self):
-        self.do_test_for_direction(Direction.backwards)
+        # self.assertFalse(horspool_search("abx", "baabac"))
+        # self.assertFalse(horspool_search("abaaaaa", "baabac")) # Needle longer than haystack.
 
 import timeit
 def benchmark_with_timeit():
     iterations = int(1e4)
     ways_of_doing_it = [
-      (direction.value, f'search(needle, haystack, {direction})') for direction in Direction
-    ] + [
+      ("horspool_search", f'horspool_search(needle, haystack)'),
       ("naive_search", 'naive_search(needle, haystack)'),
       ("branching_search", 'branching_search(needle, haystack)'),
       ("native 'in'", 'needle in haystack'),
@@ -254,8 +129,8 @@ def benchmark_with_timeit():
         ("needle contains a char not in haystack", "xab", "baccabaccabaccabaccabaccabacca" * 10),
         ("300x other chars (not in needle) and then needle at the end", "cab", ("xyz" * 100) + "cab"),
         ("pretty long needle", "cab" * 5, "baccabaccabaccabaccabaccabacca" * 10),
-        ("a lot of far-apart partial matches; interstital non-match", "apple", 100 * (100 * "x" + "app")),
-        ("a lot of far-apart partial matches; interstital match", "apple", 100 * (100 * "a" + "app")),
+        ("a lot of far-apart partial matches; interstital non-match", "apple", 10 * (10 * "x" + "app")),
+        ("a lot of far-apart partial matches; interstital match", "apple", 10 * (10 * "a" + "app")),
     ]
 
     for case_name, needle, haystack in test_cases:
@@ -267,11 +142,9 @@ def benchmark_with_timeit():
 
 import cProfile
 def benchmark_with_cprofile():
-    for direction in ['Direction.backwards', 'Direction.forwards', 'Direction.forwards_with_needle_index']:
-        print(f'{direction}')
-        cProfile.run(f'search("cab", ("xyz" * 100) + "cab", {direction})')
+    cProfile.run(f'horspool_search("cab", ("xyz" * 100) + "cab")')
 
 if __name__ == "__main__":
-    benchmark_with_timeit()
-    benchmark_with_cprofile()
+    # benchmark_with_timeit()
+    # benchmark_with_cprofile()
     unittest.main()
